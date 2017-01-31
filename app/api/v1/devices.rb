21a0e139f2cc7b884327d4346f938406aced638a
Eito Katagiri <eitoball@gmail.com>
2017-01-31 13:30:26 +0900

    disable destructive methods for now

--- app/api/v1/devices.rb
+++ app/api/v1/devices.rb
@@ -13,31 +13,32 @@ module API
           ::Device.find(params[:id])
         end
 
-        desc 'Create a device'
-        post do
-          device_params = params.select do |k, _v|
-            ::Device.valid_attributes.include?(k)
-          end
-          ::Device.create!(device_params.merge(payload: params))
-          status(201)
-        end
+        # TODO: enable when some security measurements are implemented
+        # desc 'Create a device'
+        # post do
+          # device_params = params.select do |k, _v|
+            # ::Device.valid_attributes.include?(k)
+          # end
+          # ::Device.create!(device_params.merge(payload: params))
+          # status(201)
+        # end
 
-        desc 'Update a device'
-        patch ':id' do
-          device = ::Device.find(params[:id])
-          params.delete(:id)
-          device_params = params.select do |k, _v|
-            ::Device.valid_attributes.include?(k)
-          end
-          device.update!(device_params.merge(payload: params))
-          status(204)
-        end
+        # desc 'Update a device'
+        # patch ':id' do
+          # device = ::Device.find(params[:id])
+          # params.delete(:id)
+          # device_params = params.select do |k, _v|
+            # ::Device.valid_attributes.include?(k)
+          # end
+          # device.update!(device_params.merge(payload: params))
+          # status(204)
+        # end
 
-        desc 'Delete a device'
-        delete ':id' do
-          device = ::Device.find(params[:id])
-          device.destroy!
-        end
+        # desc 'Delete a device'
+        # delete ':id' do
+          # device = ::Device.find(params[:id])
+          # device.destroy!
+        # end
       end
     end
   end
--- spec/app/api/v1/devices_spec.rb
+++ spec/app/api/v1/devices_spec.rb
@@ -40,39 +40,4 @@ describe API::V1::Devices, type: :api do
         .to be_json_including('id' => device.id)
     end
   end
-
-  context 'POST /v1/devices' do
-    it 'returns 201 Created' do
-      post '/v1/devices', { numeric_id: 74 }
-      expect(last_response.status).to eq(201)
-      expect(::Device.find_by(numeric_id: 74)).to be_a(::Device)
-    end
-  end
-
-  context 'PATCH /v1/devices/:id' do
-    let(:device) { create(:device_in_kyoto) }
-
-    it 'updates device' do
-      patch format('/v1/devices/%s', device.id), { 'location_name' => 'Kyoto' }
-      expect(last_response.status).to eq(204)
-      expect(device.reload.location_name).to eq('Kyoto')
-    end
-  end
-
-  context 'DELETE /v1/devices/:id' do
-    let(:device) { create(:device_in_kyoto) }
-
-    context 'existing device' do
-      before do
-        delete format('/v1/devices/%s', device.id)
-      end
-
-      it 'deletes specified device' do
-        expect(last_response.status).to eq(204)
-        expect do
-          ::Device.find(device.id)
-        end.to raise_exception(::ActiveRecord::RecordNotFound)
-      end
-    end
-  end
 end
