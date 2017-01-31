2605c7b2d52eb046b0c3b1b52f8b8f6f5226cd12
Eito Katagiri <eitoball@gmail.com>
2017-01-31 13:07:02 +0900

    create dynamically in device factory

--- spec/factories/device.rb
+++ spec/factories/device.rb
@@ -1,9 +1,17 @@
 FactoryGirl.define do
   factory :device do
     factory :device_in_kyoto do
-      location { format('POINT(%f %f)', 135.764331, 34.995197) }
-      location_name 'Japan, Kyoto, Kiyomizu-Gojo, MTRL'
-      payload { { nuberic_id: 74, lat: 34.995197, lon: 135.764331 } }
+      payload {
+        {
+          numeric_id: 74,
+          lat: 34.995197,
+          lon: 135.764331,
+          location_name: 'Japan, Kyoto, Kiyomizu-Gojo, MTRL'
+        }
+      }
+      numeric_id { payload[:numeric_id] }
+      location { format('POINT(%f %f)', payload[:lon], payload[:lat]) }
+      location_name { payload[:location_name] }
     end
   end
 end
