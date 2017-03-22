ce8567620608b6dcdb1c9ae13321c5c7213dcf24
Nick Dolezal <ndolezal@gmail.com>
2017-03-22 09:16:15 -0600

    Fix for rowcount in output (?)

--- cron/mapview_24h_clustered.sh
+++ cron/mapview_24h_clustered.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 source db_settings.env
-psql -f mapview_24h_clustered.sql | aws s3 cp - "${EXPORT_TARGET}"/json/view24h.json --acl public-read
+psql -f -q mapview_24h_clustered.sql | aws s3 cp - "${EXPORT_TARGET}"/json/view24h.json --acl public-read
