590ddd5b15b16f700b6c88dcf9b4c61452fae884
Mat Schaffer <mat@schaffer.me>
2017-03-23 09:39:25 +0900

    Exclude psql statement confirmation output and set max age.

--- cron/mapview_24h_clustered.sh
+++ cron/mapview_24h_clustered.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 source db_settings.env
-psql -f mapview_24h_clustered.sql | aws s3 cp - "${EXPORT_TARGET}"/json/view24h.json --acl public-read
+psql -q -f mapview_24h_clustered.sql |\
+  aws s3 cp - "${EXPORT_TARGET}"/json/view24h.json \
+  --acl public-read \
+  --cache-control "max-age=300"
