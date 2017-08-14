7c52308c486bfa267d04df63a5ff6119fbf20b22
Eito Katagiri <eitoball@gmail.com>
2017-08-15 01:34:10 +0900

    dedupe measurements from transports

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -3,6 +3,17 @@ class Measurement < ActiveRecord::Base
   self.record_timestamps = false
 
   validates :device_id, presence: true
+  validate :must_be_unique
+
+  def must_be_unique
+    if self.class.exists?(["payload - 'net_transport' - 'when_uploaded' = ?", payload_for_uniqueness.to_json])
+      errors.add(:payload, 'Must be unique')
+    end
+  end
+
+  def payload_for_uniqueness
+    payload.except('net_transport', 'when_uploaded')
+  end
 end
 
 # == Schema Information
