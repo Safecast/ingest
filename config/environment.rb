09cde4d4780a6633222bf2e3de5c402965cb4b8e
Mat Schaffer <mat@schaffer.me>
2017-09-07 14:25:16 +0900

    Suppress a few inspections intellij gets wrong

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ require 'pathname'
 require 'bundler'
 
 # Load environment settings
+# noinspection RubyConstantNamingConvention
 Config = OpenStruct.new
 Config.env = ENV['RACK_ENV'] ? ENV['RACK_ENV'].to_sym : :development
 Config.root = Pathname.new(File.expand_path('../..', __FILE__))
@@ -11,6 +12,7 @@ Bundler.require(:default, Config.env)
 
 if Config.env != :production
   require 'dotenv'
+  # noinspection RubyArgCount
   Dotenv.load(
     Config.root.join('.env.local'),
     Config.root.join(".env.#{Config.env}"),
