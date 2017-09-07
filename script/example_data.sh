e87a42f54edf50b7f24a61b8b17f060184daecdd
Mat Schaffer <mat@schaffer.me>
2017-09-07 16:46:05 +0900

    Augment example_data for posting to various envs

--- script/example_data.sh
+++ script/example_data.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env bash
 
-set -ex
+set -euxo pipefail
+
+INGEST_ENV="${1:-local}"
+
+case "${INGEST_ENV}" in
+  local)
+    URL=http://localhost:9292
+    ;;
+  dev)
+    URL=http://ingest-dev.safecast.cc
+    ;;
+  prd)
+    URL=https://ingest.safecast.org
+    ;;
+  *)
+    echo "Unknown ingest environment ${INGEST_ENV}"
+    exit 1
+esac
 
 curl -v \
   -H'Content-Type: application/json' \
-  -d'{"captured_at":"2017-01-23T00:50:18Z","device_id":1553490618,"latitude":42.564835,"longitude":-70.78382,"lora_snr":5,"lndc_cpm":13,"transport":"http:50.250.38.70:37661"}' \
-  http://localhost:9292/v1/measurements
+  -d'{"captured_at":"2017-01-23T00:50:18Z","device_id":1337,"latitude":42.564835,"longitude":-70.78382,"lora_snr":5,"lndc_cpm":13,"transport":"http:50.250.38.70:37661"}' \
+  "${URL}/v1/measurements"
 
