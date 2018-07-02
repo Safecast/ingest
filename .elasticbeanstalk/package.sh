2d481d086836c53776d01e2cd438c0f88d80a1d7
Mat Schaffer <mat@schaffer.me>
2018-07-02 22:33:40 +0900

    Less verbose aws s3 cp

--- .elasticbeanstalk/package.sh
+++ .elasticbeanstalk/package.sh
@@ -9,7 +9,7 @@ BUCKET="${3}"
 PACKAGE="${APP}-${BRANCH_NAME}-${SEMAPHORE_BUILD_NUMBER}.zip"
 
 /opt/aws-eb-cli/bin/python .elasticbeanstalk/package.py "${PACKAGE}"
-aws s3 cp ".elasticbeanstalk/app_versions/${PACKAGE}" "s3://${BUCKET}/${APP}/"
+aws s3 cp --no-progress ".elasticbeanstalk/app_versions/${PACKAGE}" "s3://${BUCKET}/${APP}/"
 aws elasticbeanstalk create-application-version \
   --region "${REGION}" \
   --application-name "${APP}" \
