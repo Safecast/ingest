5e1208321ab4c10560e6d350eac95acfbb7a749f
Andrew Todd <at@auspicacious.org>
2019-08-17 19:31:01 +0900

    Improvements from suggestions on @matschaffer's review of previous commit.

--- script/example-data.rb
+++ script/example-data.rb
@@ -3,34 +3,61 @@
 require 'csv'
 require 'json'
 require 'net/http'
+require 'optparse'
 
-api_uris = {
-  'local' => URI('http://localhost:9292/v1/measurements'),
-  'dev' => URI('http://ingest-dev.safecast.cc/v1/measurements'),
-  'prd' => URI('https://ingest.safecast.org'),
-}
+Options = Struct.new(:api_uri, :data_file, :ingest_env)
+class Parser
+  @@api_uris = {
+    'local' => URI('http://localhost:9292/v1/measurements'),
+    'dev' => URI('http://ingest-dev.safecast.cc/v1/measurements'),
+    'prd' => URI('https://ingest.safecast.org'),
+  }
+  
+  def self.parse(options)
+    args = Options.new("world")
+    args.api_uri = @@api_uris['local']
+    args.data_file = File.join(File.expand_path(File.dirname(__FILE__)), 'example-data.jsonl')
+    args.ingest_env = 'local'
 
-data_file = File.join(File.expand_path(File.dirname(__FILE__)), 'example-data.jsonl')
+    opt_parser = OptionParser.new do |opts|
+      opts.banner = "Usage: #{__FILE__} [options]"
 
-ingest_env = 'local'
-if ARGV.length == 1
-  ingest_env = ARGV[0]
-elsif ARGV.length > 1
-  STDERR.puts('You have passed more than one argument on the command line. Only one argument is accepted: the name of a known environment.')
-  exit(64)
+      opts.on('-d', '--data-file PATH', 'JSONL file containing the measurements to POST. See http://jsonlines.org/ for format.') do |data_file_arg|
+        if File.file?(data_file_arg)
+          args.data_file = File.new(data_file_arg)
+        else
+          STDERR.puts("Error: file #{data_file_arg} does not exist.")
+          exit(64)
+        end
+      end
+
+      opts.on('-e', '--ingest-environment ENV_NAME', "Name of the environment. Valid environments: #{@@api_uris.keys.to_json()}") do |ingest_env_arg|
+        if ! @@api_uris.key?(ingest_env_arg)
+          STDERR.puts('You have passed an unknown environment name. The value must be one of the following:')
+          STDERR.puts(@@api_uris.keys.to_json())
+          exit(64)
+        end
+        args.api_uri = @@api_uris[ingest_env_arg]
+        args.ingest_env = ingest_env_arg
+      end
+
+      opts.on('-h', '--help', 'Prints this help') do
+        STDERR.puts(opts)
+        exit(0)
+      end
+    end
+
+    opt_parser.parse!(options)
+    return args
+  end
 end
+options = Parser.parse(ARGV)
 
-if ! api_uris.key?(ingest_env)
-  STDERR.puts('You have passed an unknown environment name. The value must be one of the following:')
-  STDERR.puts(api_uris.keys.to_json())
-  exit(64)
-end
-
-api_uri = api_uris[ingest_env]
+api_uri = options.api_uri
+data_file = options.data_file
 
 Net::HTTP.start(api_uri.hostname, api_uri.port,
                 :use_ssl => api_uri.scheme == 'https' ) {|http|
-  # Why jsonl? See http://jsonlines.org/
   File.foreach(data_file).with_index { |line, line_num|
     begin
       parsed_measurement = JSON.parse(line)
--- script/reset-db.bash
+++ script/reset-db.bash
@@ -15,6 +15,9 @@ errmsg() {
 
 trap_exit() {
     exit_status="$?"
+    if [ -f "$script_dir"/rackup.pid ]; then
+        kill $(<"$script_dir"/rackup.pid) || true
+    fi
     if [ $exit_status -ne 0 ]; then
         set +o xtrace
         errmsg 'Something went wrong during execution.'
@@ -22,6 +25,45 @@ trap_exit() {
     fi
 }
 trap trap_exit EXIT
+
+print_help() {
+    set +o xtrace
+    errmsg 'Options:'
+    errmsg '-d <path>: Read ingest data from the file given. Mutually exclusive with -n.'
+    errmsg '-h: Print this help message.'
+    errmsg '-n: Do not pre-populate any data. Mutually exclusive with -d.'
+    set -o xtrace
+}
+
+data_file=''
+populate_data='true'
+while getopts ":d:n" opt; do
+    case $opt in
+        d)
+            data_file="$OPTARG"
+            ;;
+        h)
+            print_help
+            exit 0
+            ;;
+        n)
+            populate_data='false'
+            ;;
+        \?)
+            errmsg "Invalid option: -$OPTARG"
+            print_help
+            exit 64
+            ;;
+    esac
+done
+
+# Validate parameters
+if [ "$populate_data" != 'true' ] && [ -n "$data_file" ]; then
+    errmsg 'Error: -d and -n cannot be specified at the same time.'
+    errmsg
+    print_help
+    exit 64
+fi
 
 # This script assumes that authentication, etc. has already been set
 # up as per the system setup document referenced in the README.
@@ -32,29 +74,25 @@ if pgrep -f 'localhost:9292'; then
     exit 1
 fi
 
-# TODO this will only work in systems configured to sudo to postgres
-# without a password, which use the postgres user, etc.
-if psql -U safecast ingest-solarcast_development -c 'SELECT NULL;'; then
-    sudo -u postgres dropdb ingest-solarcast_development
-fi
-if psql -U safecast ingest-solarcast_test -c 'SELECT NULL;'; then
-    sudo -u postgres dropdb ingest-solarcast_test
-fi
-
 # Many Ruby tools assume a specific working directory... even some
 # tools that claim to support a directory parameter don't work 100%
 # without changing the working directory.
 (
     cd "$project_dir"
+    rake db:drop:all
     rake db:create
     rake db:structure:load
 
     rackup --daemonize --pid "$script_dir"/rackup.pid
 )
 
-"$script_dir"/example-data.rb
-
-kill $(<"$script_dir"/rackup.pid)
+if [ "$populate_data" = 'true' ]; then
+    if [ -n "$data_file" ]; then
+        "$script_dir"/example-data.rb -d "$data_file"
+    else
+        "$script_dir"/example-data.rb
+    fi
+fi
 
 set +o xtrace
 errmsg 'Database reset to clean state.'
