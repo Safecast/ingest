0e5b700890e2fdb2158ad27656e7ce0af4e52ea8
Andrew Todd <at@auspicacious.org>
2019-11-15 18:48:32 +0900

    Fixes to elasticbeanstalk_helper.

--- rakelib/elasticbeanstalk_helper.rb
+++ rakelib/elasticbeanstalk_helper.rb
@@ -34,16 +34,31 @@ class ElasticBeanstalkHelper
     environment_names.select { |n| n =~ /^#{environment_prefix}-#{environment_config}-/ }
   end
 
-  def current_environment_number
+  def current_environment_number(tier)
     if selected_environments.empty?
       0
     else
-      selected_environments.max.split('-').last.to_i
+      if tier == 'wrk'
+        environment_names = selected_environments.select { |n| n =~ /-wrk-\d{3}$/}
+      else
+        environment_names = selected_environments.select { |n| n !~ /-wrk-\d{3}$/}
+      end
+      if environment_names.size != 1
+        STDERR.puts('Error: More than one matching environment was found. Not sure which one is current.')
+        STDERR.puts(environment_names)
+        exit 1
+      end
+      environment_names[0].split('-').last.to_i
     end
   end
 
   def next_environment_number
-    current_environment_number + 1
+    all_numbers = (1..999).to_a
+    environment_numbers = selected_environments.map { |n| n.split('-').last.to_i }
+    puts all_numbers
+    puts environment_numbers
+    available_numbers = all_numbers - environment_numbers
+    available_numbers.min
   end
 
   def environment_name(number, tier = nil)
@@ -72,10 +87,10 @@ class ElasticBeanstalkHelper
   end
 
   def ssh_command(tier = nil)
-    %w(eb ssh) + [environment_name(current_environment_number, tier)]
+    %w(eb ssh) + [environment_name(current_environment_number(tier), tier)]
   end
 
   def deploy_command(tier = nil)
-    %w(eb deploy) + [environment_name(current_environment_number, tier)]
+    %w(eb deploy) + [environment_name(current_environment_number(tier), tier)]
   end
 end
