091b7937a67863c758343341b933f7fa60c2a40b
Andrew Todd <at@auspicacious.org>
2020-02-05 18:15:28 +0900

    Log lines are now valid JSON. Started work on machine-parseable perf data.

--- cron/logger.bash
+++ cron/logger.bash
@@ -1,6 +1,15 @@
-# File should be sourced, not executed. Do not add shebang to top.
+# This file should be sourced, not executed. Do not add shebang to top.
 
-# TODO somehwat risky to enable these in an unrelated sourced file
+# Usage:
+#
+# Most users will only need to use the log(), start_perf_timer(), and
+# stop_perf_timer() functions.
+#
+# log <LEVEL> <MESSAGE>
+# start_perf_timer <TIMER NAME>
+# stop_perf_timer <TIMER NAME>
+
+# TODO somehwat risky to enable these in an unrelated, sourced file
 set -o errexit
 set -o nounset
 set -o pipefail
@@ -10,6 +19,16 @@ command_exists() {
         printf 'true\n'
     else
         printf 'false\n'
+    fi
+}
+
+validate_log_level() {
+    # Available log levels based on SLF4J. http://www.slf4j.org/apidocs/org/slf4j/Logger.html
+    # Global associative arrays can be buggy, which is why this is declared here.
+    local -A log_levels=(['TRACE']='TRACE' ['DEBUG']='DEBUG' ['INFO']='INFO' ['WARN']='WARN' ['ERROR']='ERROR')
+    if [ -z "${log_levels["$1"]:-}" ]; then
+        printf 'Error: Log level %s is not understood, please remove it from your program.\n' 1>&2
+        exit 64
     fi
 }
 
@@ -49,27 +68,50 @@ determine_destination_enabled() {
 }
 
 configure_logger() {
+    if ! [ "$(command_exists jq)" = 'true' ]; then
+        printf 'This logger requires the jq binary to be installed. It is available in most package managers. See https://stedolan.github.io/jq/\n' 1>&2
+        exit 1
+    fi
+
+    if [ -v script_tag ]; then
+        log WARN 'The logger has already been configured. Please do not call configure_logger more than once.'
+        return
+    fi
+
+    declare -gA start_times
+
     script_tag="$1"
-    # TODO if you call this twice in the same program it will cause problems
-    logger_execution_id="$(uuidgen -r)"
+    script_tag_json_string="$(printf '%s' "$script_tag" | jq --compact-output --slurp --raw-input --monochrome-output)"
+    local logger_execution_id="$(uuidgen -r)"
+    logger_execution_id_json_string="$(printf '%s' "$logger_execution_id" | jq --compact-output --slurp --raw-input --monochrome-output)"
     use_elastic="$(determine_destination_enabled elastic)"
     use_stderr="$(determine_destination_enabled stderr)"
     use_syslog="$(determine_destination_enabled syslog)"
 }
 
-log() {
-    # Available log levels based on SLF4J. http://www.slf4j.org/apidocs/org/slf4j/Logger.html
-    local -A levels=(['TRACE']='TRACE' ['DEBUG']='DEBUG' ['INFO']='INFO' ['WARN']='WARN' ['ERROR']='ERROR')
-    local level="${levels["$1"]:?}"
-    shift 1
+generate_log_json() {
     local IFS=' '
+    local level="$1"
+    validate_log_level "$level"
+    shift 1
+    local message="$(printf '%s' "$*" | jq --compact-output --slurp --raw-input --monochrome-output)"
+    printf '{"tag":%s, "exec_id":%s, "level":"%s", "message":%s}' \
+           "$script_tag_json_string" "$logger_execution_id_json_string" "$level" "$message" \
+        | jq --compact-output --monochrome-output --sort-keys
+}
+
+write_log() {
+    local line="$1"
     if [ "$use_stderr" = 'true' ]; then
-        # TODO JSON output
-        printf 'tag:%s exec_id:%s level:%s message:%s\n' "$script_tag" "$logger_execution_id" "$level" "$*" 1>&2
+        cat 1>&2 <<< "$line"
     fi
     if [ "$use_syslog" = 'true' ]; then
-        printf 'tag: %s exec_id:%s level:%s message:%s\n' "$script_tag" "$logger_execution_id" "$level" "$*" | logger -t "$script_tag"
+        logger -t "$script_tag" <<< "$line"
     fi
+}
+
+log() {
+    write_log "$(generate_log_json $*)"
 }
 
 create_metric() {
@@ -80,15 +122,19 @@ create_metric() {
 start_perf_timer() {
     local timer_name="$1"
     local start_time="$(date -u '+%s')"
-    IFS= read -r perf_start_time_"$timer_name" <<< "$start_time"
-    log 'INFO' 'Starting timer '"$timer_name"' at '"$start_time"
+    # TODO verify variable does not already exist
+    start_times["$timer_name"]="$start_time"
+    local standard_log="$(generate_log_json INFO 'Starting timer '"$timer_name"' at '"$start_time")"
+    local timer_name_json_string="$(printf '%s' "$timer_name" | jq --compact-output --slurp --raw-input --monochrome-output)"
+    local additional_log="$(printf '{"perf_event":"START","perf_start_time":%s, "perf_timer_name":%s}' "$start_time" "$timer_name_json_string")"
+    write_log "$(printf '[%s, %s]' "$standard_log" "$additional_log" | jq --compact-output --monochrome-output --sort-keys '.[0] + .[1]')"
 }
 
 end_perf_timer() {
     local timer_name="$1"
     local end_time=$(date -u '+%s')
-    local start_time=${!perf_start_time_"$timer_name"}
-    unset perf_start_time_"$timer_name"
+    local start_time=${start_times["$timer_name"]}
+    # TODO unset
     local time_diff=$(($end_time - $start_time))
     log 'INFO' 'Ending timer '"$timer_name"' at '"$end_time"
     log 'INFO' 'Execution time for '"$timer_name"' was '"$time_diff"' seconds'
--- cron/logger_test.bash
+++ cron/logger_test.bash
@@ -3,3 +3,10 @@
 source logger.bash
 configure_logger logger_test.bash
 log INFO 'Test message'
+log INFO 'Starting perf test.'
+start_perf_timer test1
+start_perf_timer test2
+sleep 2
+end_perf_timer test1
+sleep 1
+end_perf_timer test2
